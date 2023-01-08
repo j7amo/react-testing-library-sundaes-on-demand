0556c7b48a2463cf55e35e6efa04a743f766e037
Roman Tarnakin <roman_tarnakin@epam.com>
2023-01-08 19:21:14 +0300

    add more tests for totals updating

--- src/pages/entry/test/totalUpdates.test.jsx
+++ src/pages/entry/test/totalUpdates.test.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import userEvent from '@testing-library/user-event';
 import Options from '../Options';
 import { render, screen } from '../../../test-utils/testing-library-utils';
+import OrderEntry from '../OrderEntry';
 
 test('scoops subTotal starts with 0 and updates when scoops number changes', async () => {
   const user = userEvent.setup();
@@ -43,4 +44,96 @@ test('scoops subTotal starts with 0 and updates when scoops number changes', asy
   await user.type(chocolateInput, '2');
 
   expect(scoopsSubtotal).toHaveTextContent('6');
+});
+
+test('toppings subTotal starts with 0 and updates when scoops number changes', async () => {
+  const user = userEvent.setup();
+  render(<Options optionType="toppings" />);
+
+  const toppingsTotal = screen.getByText('Toppings total: $', { exact: false });
+  expect(toppingsTotal).toHaveTextContent('0.00');
+
+  const hotFudgeToppingCheckbox = await screen.findByRole('checkbox', {
+    name: /hot fudge/i,
+  });
+  await user.click(hotFudgeToppingCheckbox);
+
+  expect(toppingsTotal).toHaveTextContent('1.50');
+
+  const mNmsToppingCheckbox = await screen.findByRole('checkbox', {
+    name: /M&Ms/i,
+  });
+  await user.click(mNmsToppingCheckbox);
+
+  expect(toppingsTotal).toHaveTextContent('3.00');
+
+  await user.click(mNmsToppingCheckbox);
+  expect(toppingsTotal).toHaveTextContent('1.50');
+});
+
+describe('grand total', () => {
+  test('starts with $0.00', async () => {
+    render(<OrderEntry />);
+
+    const grandTotal = screen.getByText('Grand total: $', { exact: false });
+    expect(grandTotal).toHaveTextContent('0.00');
+  });
+
+  test('updates correctly if scoop is added first', async () => {
+    const user = userEvent.setup();
+    render(<OrderEntry />);
+
+    const grandTotal = screen.getByText('Grand total: $', { exact: false });
+
+    const vanillaInput = await screen.findByRole('spinbutton', {
+      name: 'Vanilla',
+    });
+
+    await user.clear(vanillaInput);
+    await user.type(vanillaInput, '1');
+
+    expect(grandTotal).toHaveTextContent('2.00');
+  });
+
+  test('updates correctly if topping is added first', async () => {
+    const user = userEvent.setup();
+    render(<OrderEntry />);
+
+    const grandTotal = screen.getByText('Grand total: $', { exact: false });
+
+    const mNmsToppingCheckbox = await screen.findByRole('checkbox', {
+      name: /M&Ms/i,
+    });
+    await user.click(mNmsToppingCheckbox);
+
+    expect(grandTotal).toHaveTextContent('1.50');
+  });
+
+  test('resets to $0.00 if all items are removed', async () => {
+    const user = userEvent.setup();
+    render(<OrderEntry />);
+
+    const grandTotal = screen.getByText('Grand total: $', { exact: false });
+
+    const vanillaInput = await screen.findByRole('spinbutton', {
+      name: 'Vanilla',
+    });
+
+    const mNmsToppingCheckbox = await screen.findByRole('checkbox', {
+      name: /M&Ms/i,
+    });
+
+    await user.clear(vanillaInput);
+    await user.type(vanillaInput, '1');
+    await user.click(mNmsToppingCheckbox);
+
+    expect(grandTotal).toHaveTextContent('3.50');
+
+    await user.click(mNmsToppingCheckbox);
+    expect(grandTotal).toHaveTextContent('2.00');
+
+    await user.clear(vanillaInput);
+    await user.type(vanillaInput, '0');
+    expect(grandTotal).toHaveTextContent('0.00');
+  });
 });
