6e1dda4a5e16fea15960605be0220a2253436365
Roman Tarnakin <roman_tarnakin@epam.com>
2023-01-08 19:20:35 +0300

    fix OrderEntry components

--- src/pages/entry/OrderEntry.jsx
+++ src/pages/entry/OrderEntry.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Options from './Options';
+import { useOrderDetails } from '../../contexts/OrderDetails';
+import formatCurrency from '../../utils';
 
 function OrderEntry() {
+  const { totals } = useOrderDetails();
+
   return (
     <>
       <Options optionType="scoops" />
       <Options optionType="toppings" />
+      <h2>
+        {`Grand total: ${formatCurrency(totals.scoops + totals.toppings)}`}
+      </h2>
     </>
   );
 }
