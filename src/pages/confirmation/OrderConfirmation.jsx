6e7648e26dc9c5884cdb2cc60f9c298a68c8aa24
Roman Tarnakin <roman_tarnakin@epam.com>
2023-01-09 18:14:55 +0300

    add OrderConfirmation page

---
+++ src/pages/confirmation/OrderConfirmation.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import * as PropTypes from 'prop-types';
+import { Button, Container } from 'react-bootstrap';
+import { ORDER_PHASES } from '../../const/const';
+import { useOrderDetails } from '../../contexts/OrderDetails';
+
+export default function OrderConfirmation({ setOrderPhase }) {
+  const { orderNumber, resetOrderCounts } = useOrderDetails();
+
+  const buttonClickHandler = () => {
+    resetOrderCounts();
+    setOrderPhase(ORDER_PHASES.IN_PROGRESS);
+  };
+
+  return (
+    <Container style={{ textAlign: 'center' }}>
+      <h2>Thank you!</h2>
+      <p>{`Your order number is ${orderNumber}`}</p>
+      <Button variant="primary" type="button" onClick={buttonClickHandler}>
+        New order
+      </Button>
+    </Container>
+  );
+}
+
+OrderConfirmation.propTypes = {
+  setOrderPhase: PropTypes.func.isRequired,
+};
